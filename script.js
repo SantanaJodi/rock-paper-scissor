943572243481646c0b325fdbaabacbb78f7ac787
SantanaJodi <targetsan21@gmail.com>
2019-11-01 22:38:08 +0700

    tambahan untuk mediaquery

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+var pPick;
+
 function rock(){
   var rdoc = document.getElementById('rock')
   rdoc.style.backgroundColor='#fafafa';
+  document.getElementById('paper').style.display='none';
+  document.getElementById('scissor').style.display='none';
+  pPick = 1;
+}
+
+function paper(){
+  var rdoc = document.getElementById('paper')
+  rdoc.style.backgroundColor='#fafafa';
+  document.getElementById('rock').style.display='none';
+  document.getElementById('scissor').style.display='none';
+  pPick = 2;
+}
+
+function scissor(){
+  var rdoc = document.getElementById('scissor')
+  rdoc.style.backgroundColor='#fafafa';
+  document.getElementById('rock').style.display='none';
+  document.getElementById('paper').style.display='none';
+  pPick = 3;
 }

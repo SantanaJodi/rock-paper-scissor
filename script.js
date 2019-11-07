592f0aed95c81dfd5e53fec8968301e9ccb50958
SantanaJodi <targetsan21@gmail.com>
2019-11-08 00:37:49 +0700

    Finishing

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-var pPick;
+function button(pick, unpickA, unpickB){
+  var cPick = Math.floor(Math.random()*3) + 1;
+  // var cPick = 4;
+  var compP;
+  if (cPick == 1){
+    compP = 'rock';
+  } else if (cPick == 2){
+    compP = 'paper';
+  } else if (cPick == 3){
+    compP = 'scissor';
+  }
 
-function rock(){
-  var rdoc = document.getElementById('rock')
-  rdoc.style.backgroundColor='#fafafa';
-  document.getElementById('paper').style.display='none';
-  document.getElementById('scissor').style.display='none';
-  pPick = 1;
+  document.getElementById(pick).style.backgroundColor='#fafafa';
+  document.getElementById(pick).onclick='none';
+  document.getElementById(unpickA).style.display='none';
+  document.getElementById(unpickB).style.display='none';
+  document.getElementById('result').style.display='block';
+  document.getElementById('resPlace').style.display='none';
+  document.getElementById('button').style.display='block';
+
+  console.log('Math shows ' + cPick);
+  console.log('Computer choose ' + comp);
+
+  var comp = document.getElementById('comp');
+  var juri = document.getElementById('juri');
+  if (pick == compP){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'DRAW'
+  } else if (pick == 'rock' && compP == 'paper'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER LOSE'
+  } else if (pick == 'rock' && compP == 'scissor'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER WIN'
+  } else if (pick == 'paper' && compP == 'scissor'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER LOSE'
+  } else if (pick == 'paper' && compP == 'rock'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER WIN'
+  } else if (pick == 'scissor' && compP == 'rock'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER LOSE'
+  } else if (pick == 'scissor' && compP == 'paper'){
+    comp.innerHTML = compP.toUpperCase();
+    juri.innerHTML = 'PLAYER WIN'
+  } else {
+    document.getElementById('result').innerHTML = 'Something is wrong in the source-code, computer cant decide what to pick';
+    document.getElementById('result').style.color='red';
+  }
 }
 
-function paper(){
-  var rdoc = document.getElementById('paper')
-  rdoc.style.backgroundColor='#fafafa';
-  document.getElementById('rock').style.display='none';
-  document.getElementById('scissor').style.display='none';
-  pPick = 2;
-}
-
-function scissor(){
-  var rdoc = document.getElementById('scissor')
-  rdoc.style.backgroundColor='#fafafa';
-  document.getElementById('rock').style.display='none';
-  document.getElementById('paper').style.display='none';
-  pPick = 3;
+function reload(){
+  location.reload();
 }
